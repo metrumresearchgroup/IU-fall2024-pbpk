e5fd5c89ff2ab1356bbe57404b2170627c5b27e1
Kyle Baron <kyleb@metrumrg.com>
2022-09-23 11:38:39 -0500

    updates

--- day1/exercises/pbpk-rifampin-ddi.R
+++ day1/exercises/pbpk-rifampin-ddi.R
@@ -8,7 +8,7 @@ source(here("day1/src/global.R"))
 #' - model file: `day1/model/rifampicin_midazolam.cpp`
 #' - look at the model, parameters and compartments
 #' - Dosing regimen 1
-#'   - rifampin 600 mg po daily x7 `then`
+#'   - rifampin 600 mg po daily x10 `then`
 #'   - midazolam 3 mg x1 12 hours after last rifampin dose
 #' - Dosing regimen 2
 #'   - midazolam 3 mg x1 administered at the same time as 
@@ -18,31 +18,13 @@ source(here("day1/src/global.R"))
 #'     
 #' - Plot rifampin concentraion over time for regimen 1
 #' - Plot midazolam concentration over time for regimen 1 and 2
+#'
+#' - Outputs
+#'  - Ccentral - rifampin concentration
+#'  - mCcentral - midazolam concentration
 #' 
 
-mod <- mread(here("day1/model/rifampicin_midazolam.cpp"))
+mod <- mread("rifampicin_midazolam", here("day1/model"))
 
 
-rif <- ev(amt = 600, ii = 24, addl = 6, cmt = 1)
-mid <- ev(amt = 3, cmt = 2, time = 12)
-
-dose1 <- seq(rif, mid) %>% mutate(reg = "ddi")
-dose2 <- filter(dose1, amt==3) %>% mutate(reg = "mono")
-
-
-dose <- as_data_set(dose2, dose1)
-
-
-out <- mrgsim(mod, events = dose, end = 240, delta = 0.1, 
-              recover = "reg")
-
-plot(out, "Ccentral")
-
-out2 <- filter_sims(out, time >= 168)
-
-plot(out2, "mCcentral")
-
-mod %>% ev(dose) %>% 
-  mrgsim(end =240, delta = 0.1) %>% 
-  plot("Ccentral,mCcentral")
 
