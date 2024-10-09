52ac009e66a8247d740f64899baeb9a9374ad786
ahmede <ahmede@metrumrg.com>
2024-10-09 00:18:43 -0400

    update gcsf

--- day1/exercises/answers/gcsf_answer.R
+++ day1/exercises/answers/gcsf_answer.R
@@ -10,11 +10,10 @@ rm(list = ls())
 #' - Model: model/gcsf.cpp
 #' 
 #' - Simulate 2.5, 5, and 10 mcg/kg assuming 50, 70, 90 kg individual
-#' - Simulate 2.5, 5, and 10 mcg/kg assuming log(WT) ~ N(log(80),0.1)
 #' 
 #' - Do daily SC dosing x 7d
 
-mod <- mread_cache("gcsf", "model") %>% zero_re()
+mod <- mread_cache("gcsf", here("day1","model")) %>% zero_re()
 
 data <- 
   expand.ev(
--- day1/exercises/gcsf.R
+++ day1/exercises/gcsf.R
@@ -10,7 +10,6 @@ rm(list = ls())
 #' - Model: model/gcsf.cpp
 #' 
 #' - Simulate 2.5, 5, and 10 mcg/kg assuming 50, 70, 90 kg individual
-#' - Simulate 2.5, 5, and 10 mcg/kg assuming log(WT) ~ N(log(80),0.1)
 #' 
 #' - Do daily SC dosing x 7d
 
